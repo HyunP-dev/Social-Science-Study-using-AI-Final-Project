467d1acbed90dd940c105c55f6ac41c2c34d26b6
Hyun Park <hyunp.dev@gmail.com>
2022-06-09 19:05:45 +0900

    upload

--- main.r
+++ main.r
@@ -12,41 +12,44 @@ text_classify <- import("model")$text_classify
 
 plan(multicore, workers = 8)
 
+httr::set_config(httr::use_proxy("socks5://localhost:9050"))
+httr::reset_config()
 
-# 이걸 쓰자!
-seq(
-    as.Date("2015/01/01"),
-    by = "week",
-    length.out = (365 * 7 + 2) / 7) -> ds
 
-# 이건 데이터셋이 너무 많아져서 쬬큼...
-seq(
-    as.Date("2015/01/01"),
-    by = "day",
-    length.out = 10) -> ds
 
-df <- do.call(rbind, future_Map(function(date) {
-    data.frame(
-        year = as.integer(format(date, "%Y")),
-        month = as.integer(format(date, "%m")),
-        week = as.integer(format(date, "%U")),
-        get_news_list(format(date, "%Y%m%d"), F))
-}, ds))
+curr <- dbGetQuery(conn, "SELECT url FROM scraped_urls")
+targets <- Filter(function(url) which(curr$url == url) == 0, df$url)
+pb <- progress_bar$new(total = length(targets))
+for (url in targets) {
+    do.call(
+        rbind.data.frame,
+        future_Map(function(page) {
+            tryCatch(
+                expr = get_news_comments(url, page),
+                error = function(e) data.frame()
+            )
+        }, 1:5)
+    ) -> comments_df
+    if (nrow(comments_df) == 0) next
+    dbWriteTable(conn, "comments",
+        cbind(url = url, comments_df),
+        append = TRUE
+    )
+    pb$tick()
+}
 
-do.call(
-    rbind.data.frame,
-    future_Map(function(page) {
-        "https://news.nate.com/view/20220530n31323?mid=n1006" |>
-            get_news_comments(page)
-    }, 1:5)
-) -> comments_df
 comments_df$text |>
     text_classify() -> cfd_df
-cfd_df |> sapply(mean) |> barplot()
+cfd_df |>
+    sapply(mean) |>
+    barplot()
 
 
 library(DBI)
 library(RSQLite)
 conn <- dbConnect(RSQLite::SQLite(), "./natenews.sqlite")
-dbWriteTable(conn, "week_articles", df, append = TRUE)
 
+
+
+
+dbGetQuery(conn, "SELECT * FROM comments") |> View()
---
+++ scrap.r
@@ -0,0 +1,34 @@
+library(future)
+library(future.apply)
+library(DBI)
+library(RSQLite)
+library(tidyverse)
+
+
+plan(multicore, workers = 8)
+
+conn <- dbConnect(RSQLite::SQLite(), "./natenews.sqlite")
+
+# 2015년부터 향후 7년 간 주 간격으로 시간 샘플링.
+seq(
+    as.Date("2015/01/01"),
+    by = "week",
+    length.out = (365 * 7 + 2) / 7
+) -> ds
+
+# tor proxy를 이용.
+httr::set_config(httr::use_proxy("socks5://localhost:9050"))
+
+df <- do.call(rbind, future_Map(function(date) {
+    data.frame(
+        year = as.integer(format(date, "%Y")),
+        month = as.integer(format(date, "%m")),
+        week = as.integer(format(date, "%U")),
+        get_news_list(format(date, "%Y%m%d"), F)
+    )
+}, ds))
+
+dbWriteTable(conn, "week_articles", df, append = TRUE)
+
+
+
